c6efd23023b551e2f678c243692ff1a2125473d9
Belhassen Limam <belhassen.limam-extern@renault.com>
2024-10-27 16:56:38 +0100

    Initial Commit

---
+++ testIban/testIbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  testIbanApp.swift
+//  testIban
+//
+//  Created by Belhassen LIMAM on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct testIbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testIbanTests/testIbanTests.swift
@@ -0,0 +1,17 @@
+//
+//  testIbanTests.swift
+//  testIbanTests
+//
+//  Created by Belhassen LIMAM on 27/10/2024.
+//
+
+import Testing
+@testable import testIban
+
+struct testIbanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
